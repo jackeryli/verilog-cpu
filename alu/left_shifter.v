d9e9c6cb61381beb57f126c657d58ebe58115071
Vchivalry <149724419+Vchivalry@users.noreply.github.com>
2023-11-05 15:16:50 -0800

    left_shifter.v

--- alu/left_shifter.v
+++ alu/left_shifter.v
@@ -2,5 +2,5 @@ module left_shifter (In, Out);
     input [31:0] In;
     output wire [31:0] Out;
     parameter SR_Bit = 1;
-    assign Out = {In[31-SR_Bit:0], SR_Bit{1'b0}};
+    assign Out = {In[31-SR_Bit:0], {SR_Bit{1'b0}}};
 endmodule
