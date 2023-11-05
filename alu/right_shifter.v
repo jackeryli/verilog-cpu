f2a7eba8662aa1f71fc9bcdf3860393aaf261911
Vchivalry <149724419+Vchivalry@users.noreply.github.com>
2023-11-05 15:18:55 -0800

    right_shifter.v

--- alu/right_shifter.v
+++ alu/right_shifter.v
@@ -2,5 +2,5 @@ module right_shifter (In, Out);
     input [31:0] In;
     output wire [31:0] Out;
     parameter SR_Bit = 1;
-    assign Out = {SR_Bit{1'b0}, In[31:SR_Bit]};
+    assign Out = {{SR_Bit{1'b0}}, In[31:SR_Bit]};
 endmodule
