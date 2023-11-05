b135f37fbc8d11d181caf6e203327f00d7785464
Russell"Rosé“Zhang <35621220+ultraJang@users.noreply.github.com>
2023-11-04 19:47:49 -0700

    Add files via upload

--- ram.v
+++ ram.v
@@ -1,43 +1,30 @@
 module RAM_2_16x32 (
     data_input,
     data_output,
-    // data_io,
     write_enable,
     read_enable,
     address
-    // clk
 );
 parameter words_num = 32, address_bus = 16;
 integer i=0;
-// input clk,rst;
 input [words_num-1:0] data_input;
 input [address_bus-1:0] address;
 input write_enable, read_enable;
 output reg [words_num-1:0] data_output;
-// reg [words_num-1:0] WriteRAM;
 
-// inout [words_num-1:0] data_io;
 
-reg [words_num-1:0] memory [0:(1<<address_bus)]; //32-bits memory with 2^16 words
+reg [words_num-1:0] memory [0:(1<<address_bus)];
 
-// always @(posedge clk or posedge rst)
 always @(*)
     begin
-        // if(rst) begin
-        //     for (i=0;i<(1<<address_bus);i=i+1) begin
-        //         memory[i]<=32'b0;
-        //     end
-        // end
         if (write_enable) begin
-            memory[address] <= data_input;
-            // data_output <= 32'b0;
+            memory[address] = data_input;
         end
         else if (read_enable) begin
-            data_output <= memory[address];
+            data_output = memory[address];
         end
         else begin
-            data_output <= 32'b0;
+            data_output = 32'b0;
         end
     end
-//  assign data_output = read_enable? WriteRAM: 32'bz;
 endmodule
--- ram_test.v
+++ ram_test.v
@@ -1,39 +1,28 @@
 module RAM_tb;
-//use register for testbench
-// reg clk, rst;
 reg write_enable, read_enable;
 reg [15:0] address;
-// reg [31:0] in_out;
 reg [31:0] data_input;
 wire [31:0] data_output;
-// reg [31:0] WriteRAM;
 
 RAM_2_16x32 uut(
-    // .data_io(in_out),
     .write_enable(write_enable),
-    // .WriteRAM(WriteRAM),
     .data_input(data_input),
     .data_output(data_output),
     .read_enable(read_enable),
     .address(address)
-    // .clk(clk),
-    // .rst(rst)
 );
 
 initial begin
-    // $monitor($time,,,
-    // "Data=%d",data_output);
 data_input = 0;
 write_enable = 0;
 read_enable = 0;
 
-#10//write enable test
-data_input = 20; //in adr 30 write 10
+#10
+data_input = 20;
 address = 66;
 write_enable = 0;
 
 #10
-//read adr 30
 write_enable = 1;
 
 #10
@@ -49,13 +38,11 @@ address = 55;
 write_enable = 1;
 
 #10
+write_enable = 0;
 read_enable = 1;
 #10
 read_enable = 0;
 
 
-// $finish();
 end
-
-// always #5 clk = ~clk;
 endmodule
