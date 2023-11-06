9c536b6d89422db5a5403ef1a779b572804b4e28
Russell"Rosé“Zhang <35621220+ultraJang@users.noreply.github.com>
2023-11-06 14:55:55 -0800

    Update ram_test.v

--- ram/ram_test.v
+++ ram/ram_test.v
@@ -1,6 +1,6 @@
 module RAM_tb;
 reg RW;
-reg [15:0] address;
+reg [31:0] address;
 reg [31:0] data_input;
 wire [31:0] data_output;
 
@@ -14,27 +14,33 @@ RAM_2_16x32 uut(
 initial begin
     #5 RW=1; address=65536'd0; data_input=32'hAAAAAAA0;
     #5 RW=1; address=65536'd1; data_input=32'hAAAAAAA1;
+    
     #5 RW=1; address=65536'd2; data_input=32'hAAAAAAA2;
+    
     #5 RW=1; address=65536'd3; data_input=32'hAAAAAAA3;
+    
     #5 RW=1; address=65536'd4; data_input=32'hAAAAAAA4;
+    
     #5 RW=1; address=65536'd5; data_input=32'hAAAAAAA5;
+    
     #5 RW=1; address=65536'd6; data_input=32'hAAAAAAA6;
+    
     #5 RW=1; address=65536'd7; data_input=32'hAAAAAAA7;
 
     #100
 
     $writememh("ram_data.txt", uut.memory);
+
     #100
+    #5 RW=0; address=65536'd0; #5 $display("Memory at %d: %h", address, data_output);
+    #5 RW=0; address=65536'd1; #5 $display("Memory at %d: %h", address, data_output);
+    #5 RW=0; address=65536'd2; #5 $display("Memory at %d: %h", address, data_output);
+    #5 RW=0; address=65536'd3; #5 $display("Memory at %d: %h", address, data_output);
+    #5 RW=0; address=65536'd4; #5 $display("Memory at %d: %h", address, data_output);
+    #5 RW=0; address=65536'd5; #5 $display("Memory at %d: %h", address, data_output);
+    #5 RW=0; address=65536'd6; #5 $display("Memory at %d: %h", address, data_output);
+    #5 RW=0; address=65536'd7; #5 $display("Memory at %d: %h", address, data_output);
 
-    #5 RW=0; address=65536'd0; #5 $display("Mermoy at %d: %h", address, data_output);
-    #5 RW=0; address=65536'd1; #5 $display("Mermoy at %d: %h", address, data_output);
-    #5 RW=0; address=65536'd2; #5 $display("Mermoy at %d: %h", address, data_output);
-    #5 RW=0; address=65536'd3; #5 $display("Mermoy at %d: %h", address, data_output);
-    #5 RW=0; address=65536'd4; #5 $display("Mermoy at %d: %h", address, data_output);
-    #5 RW=0; address=65536'd5; #5 $display("Mermoy at %d: %h", address, data_output);
-    #5 RW=0; address=65536'd6; #5 $display("Mermoy at %d: %h", address, data_output);
-    #5 RW=0; address=65536'd7; #5 $display("Mermoy at %d: %h", address, data_output);
-
-    $finish;
+    // $finish;
 end
 endmodule
