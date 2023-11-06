ab9761b0a0a8d2ee8a72760ad93831e3d2c6721e
lbaoligei <67539405+lbaoligei@users.noreply.github.com>
2023-11-05 22:07:43 -0800

    Fixed compare

--- alu/cmp.v
+++ alu/cmp.v
@@ -1,18 +1,16 @@
-module cmp (In1, In2, flag);
-input [31:0] In1, In2;
-output reg [3:0] flag;
-integer result;
-assign result = In1-In2;
-always @ * begin
-    if (result < 0)
-        flag = 4'b1000;
-    else if (result == 0)
-        flag = 4'b0100;
-    else if (result >= 1<<32)
-        flag = 4'b0010;
-    else if ((result > 0 && In1<0 && In2<0)||(result<0 && In1>0 && In2>0))
-        flag = 4'b0001;
-    else
-        flag = 4'b0000;
-end
+module cmp (In1, In2, flag, S);
+input signed [31:0] In1, In2;
+input S;
+output [3:0] flag;
+wire signed [31:0] result, negIn2, sum;
+wire carry;
+
+assign negIn2 = ~In2 + 1; // Taking two's complement for subtraction
+assign result = In1 + negIn2;
+assign {carry, sum} = In1 + In2;
+
+assign flag[3] = result[31];
+assign flag[2] = (result == 32'b0);
+assign flag[1] = carry;
+assign flag[0] = (In1[31] ~^ In2[31]) & (sum[31] ^ In1[31]);
 endmodule
