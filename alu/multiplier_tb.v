37cbf642826d3249c021fa16f16ea128d6e83776
Vchivalry <149724419+Vchivalry@users.noreply.github.com>
2023-11-25 20:22:25 -0800

    Update multiplier_tb.v

--- alu/multiplier_tb.v
+++ alu/multiplier_tb.v
@@ -4,7 +4,7 @@ module multiplier_tb;
     wire Overflow;
 
     // Instantiate the adder module
-    adder uut (
+    multiplier uut (
         .In1(In1),
         .In2(In2),
         .Out(Out),
@@ -16,25 +16,25 @@ module multiplier_tb;
         In1 = 5;
         In2 = 7;
         #10;
-        $display("Test Case 1: In1 = %d, In2 = %d, Out = %b, Overflow = %b", In1, In2, Out,Overflow);
+        $display("Test Case 1: In1 = %d, In2 = %d, Out = %b, out = %d, Overflow = %b", In1, In2, Out,Out,Overflow);
 
         // Test case 2: Negative numbers
-        In1 = -10;
+        In1 = 1;
         In2 = -5;
         #10;
-        $display("Test Case 2: In1 = %d, In2 = %d, Out = %b, Overflow = %b", In1, In2, Out, Overflow);
+        $display("Test Case 2: In1 = %d, In2 = %d, Out = %b, out = %d, Overflow = %b", In1, In2, Out,Out,Overflow);
 
         // Test case 3: Overflow test
         In1 = 2147483647; // Maximum positive signed 32-bit number
-        In2 = 2147483647;
+        In2 = 1999999999;
         #10;
-        $display("Test Case 3: In1 = %d, In2 = %d, Out = %b,Overflow = %b", In1, In2, Out, Overflow);
+        $display("Test Case 3: In1 = %d, In2 = %d, Out = %b, out = %d, Overflow = %b", In1, In2, Out,Out,Overflow);
 
         // Test case 4: Negative overflow test
         In1 = -2147483648; // Minimum negative signed 32-bit number
         In2 = -1;
         #10;
-        $display("Test Case 4: In1 = %d, In2 = %d, Out = %b, Overflow = %b", In1, In2, Out, Overflow);
+        $display("Test Case 4: In1 = %d, In2 = %d, Out = %b, out = %d, Overflow = %b", In1, In2, Out,Out,Overflow);
 
         // Add more test cases as needed
 
